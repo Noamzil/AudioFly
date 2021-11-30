15824d77ec75978503bd194a10e0a5db3bffa319
Noam Zilberman <noam@zilberman.org>
2021-11-30 21:36:32 +0200

    fixed router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <section class="main-app-container">
-    <app-header></app-header>
-    <div id="app" hidden>
-      <div id="nav">
-      </div>
+    <app-header/>
+    <div id="app">
       <router-view />
     </div>
     <app-side-bar/>
-    <app-footer></app-footer>
+    <app-footer/>
   </section>
 </template>
 
 <script>
 import appHeader from './components/app-header.cmp.vue';
-
 import appSideBar from './components/side-bar.cmp.vue'
 import appFooter from './components/app-footer.cmp.vue';
 
--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,5 +1,7 @@
-<template>
-  
+<template>  
+<section>
+    <h1>Home</h1>
+</section>
 </template>
 
 <script>
