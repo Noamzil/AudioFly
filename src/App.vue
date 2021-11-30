3b8e3cb1acf6b1bcc893c4689ce84a5cc9096c7a
Or Ziskovich <zisko1992@gmail.com>
2021-11-30 15:20:56 +0200

    started with header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
+
+
 <template>
+<section>
+<app-header></app-header>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -6,27 +10,17 @@
     </div>
     <router-view/>
   </div>
+</section>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import appHeader from './components/app-header.cmp.vue'
+export default {
+  components: {
+    appHeader
   }
 }
-</style>
+</script>
+
+
+
---
+++ src/components/app-header.cmp.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+      <h1>header</h1>
+      <router-link to="/user"></router-link>
+      <div>
+      <button>back</button>
+      <button>next</button>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'app-header',
+}
+</script>
+
