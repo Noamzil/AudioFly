07ee917255e323bfe46994db31123dc8f12666a9
Or Ziskovich <zisko1992@gmail.com>
2021-11-30 19:36:46 +0200

    user nav modal

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@
     <app-header></app-header>
     <div id="app">
       <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
       </div>
       <router-view />
     </div>
