1e31683fc9340a23c5d6450f76257e7fa22fafb1
Elior Papo <eliorpapo3@gmail.com>
2021-11-30 17:05:43 +0200

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
-
-
 <template>
-<section>
-<app-header></app-header>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <section>
+    <app-header></app-header>
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
     </div>
-    <router-view/>
-  </div>
-</section>
+    <app-footer></app-footer>
+  </section>
 </template>
 
 <script>
-import appHeader from './components/app-header.cmp.vue'
+import appHeader from './components/app-header.cmp.vue';
+
+
+import appFooter from './components/app-footer.cmp.vue';
+
 export default {
   components: {
-    appHeader
-  }
-}
+    appHeader,
+    appFooter,
+  },
+};
 </script>
-
-
-
