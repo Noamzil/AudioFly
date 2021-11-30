040e8ea4d5d957217db773f43367119c99554cd7
Or Ziskovich <zisko1992@gmail.com>
2021-11-30 21:41:52 +0200

    small touch header

--- src/components/app-header.cmp.vue
+++ src/components/app-header.cmp.vue
@@ -1,10 +1,18 @@
 <template>
     <header class="main-header">
-        <user-nav></user-nav>
         <div>  
-              <button @click="nextHistory"><svg role="img" focusable="false" viewBox="0 0 24 24" ><polyline points="16 4 7 12 16 20" fill="none"></polyline></svg></button>
-              <button @click="prevHistory"><svg role="img" focusable="false" viewBox="0 0 24 24" ><polyline points="8 4 17 12 8 20" fill="none"></polyline></svg></button>
+              <button @click="nextHistory" :class="isNext?'active':''">
+                  <svg role="img" focusable="false" viewBox="0 0 24 24" >
+                      <polyline points="16 4 7 12 16 20" fill="none"></polyline>
+                  </svg>
+              </button>
+              <button @click="prevHistory" :class="isPrev?'active':''">
+                  <svg role="img" focusable="false" viewBox="0 0 24 24" >
+                      <polyline points="8 4 17 12 8 20" fill="none"></polyline>
+                  </svg>
+              </button>
         </div>
+                <user-nav></user-nav>
     </header>
 </template>
 
@@ -12,11 +20,19 @@
 import userNav  from './user-nav.cmp.vue';
 export default {
     name: 'app-header',
+    data() {
+        return {
+            isNext: false,
+            isPrev: false,
+        }
+    },
     methods: {
         nextHistory() {
+            this.isNext = !this.isNext
             console.log('Going to the next page on your history');
         },
         prevHistory() {
+            this.isPrev = !this.isPrev
             console.log('Going to the previous page on your history');
         }
     },
--- src/components/user-nav.cmp.vue
+++ src/components/user-nav.cmp.vue
@@ -1,12 +1,12 @@
 <template>
 <section>
-  <div class="user-nav" @click="isNav=!isNav">
+  <div class="user-nav" :class="isNav ? 'clicked' : '' " @click="isNav=!isNav">
         <svg role="img" viewBox="0 0 16 16"><path d="M3 6l5 5.794L13 6z"></path></svg>
         <h3>{{username}}</h3>
-        <avatar :username="username"></avatar>
+        <avatar :size="35" :username="username"></avatar>
   </div>
   <nav class="user-nav-modal" v-if="isNav">
-      <router-link to="/">Account</router-link>
+      <router-link to="/">Account <svg role="img" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 7V1H9v1h4.29L7.11 8.18l.71.71L14 2.71V7h1zM1 15h12V9h-1v5H2V4h5V3H1v12z" clip-rule="evenodd"></path></svg></router-link>
       <router-link to="/">Profile</router-link>
       <router-link to="/">Log out</router-link>
   </nav>
