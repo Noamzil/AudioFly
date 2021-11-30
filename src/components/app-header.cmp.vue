910efb056e8d290753dcd46e93c7f32cc6221abd
Or Ziskovich <zisko1992@gmail.com>
2021-11-30 17:06:31 +0200

    starting on header

--- src/components/app-header.cmp.vue
+++ src/components/app-header.cmp.vue
@@ -1,12 +1,13 @@
 <template>
-  <header>
-      <h1>header</h1>
-      <router-link to="/user"></router-link>
-      <div>
-      <button>back</button>
-      <button>next</button>
-      </div>
-  </header>
+    <header class="main-header">
+        <h1>header</h1>
+        <router-link to="/user"></router-link>  
+        <div>  
+              <button><svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" class="Svg-ytk21e-0 be  vrDs aHN1vHt_AzvQXKPGAGn6"><polyline points="16 4 7 12 16 20" fill="none" stroke="#181818"></polyline></svg></button>
+              <button><svg role="img" focusable="false" height="24" width="24" viewBox="0 0 24 24" class="Svg-ytk21e-0 bevrDs aHN1vHt_AzvQXKPGAGn6"><polyline points="8 4 17 12 8 20" fill="none" stroke="#181818"></polyline></svg></button>
+    
+        </div>
+    </header>
 </template>
 
 <script>
