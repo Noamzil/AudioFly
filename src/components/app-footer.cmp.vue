ae08651013393426839aac094a2cadb007ed8345
Elior Papo <eliorpapo3@gmail.com>
2021-12-01 00:21:43 +0200

    first version of footer -elior

--- src/components/app-footer.cmp.vue
+++ src/components/app-footer.cmp.vue
@@ -2,12 +2,14 @@
   <section class="footer">
     <played-note-details />
     <song-progress />
+    <device-control />
   </section>
 </template>
 
 <script>
 import playedNoteDetails from './footer-cmps/played-note-details.cmp.vue';
 import songProgress from './footer-cmps/song-progress.cmp.vue';
+import deviceControl from './footer-cmps/device-control.cmp.vue';
 
 export default {
   name: 'app-footer',
@@ -17,6 +19,7 @@ export default {
   components: {
     playedNoteDetails,
     songProgress,
+    deviceControl,
   },
 };
 </script>
