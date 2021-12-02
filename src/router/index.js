109f03c3043ab5ef0563c62bc762bcb7666f146a
Elior Papo <eliorpapo3@gmail.com>
2021-12-02 04:12:34 +0200

    youtube api

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+import { Result } from 'element-ui';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
@@ -19,7 +20,6 @@ const routes = [
     name: 'playlist',
     component: () => import('../pages/playlist-page.vue'),
   },
-
 ];
 
 const router = new VueRouter({
@@ -27,3 +27,23 @@ const router = new VueRouter({
 });
 
 export default router;
+
+var YOUTUBEAPIKEY = 'AIzaSyCAwn2YlmL22UvEHK4xAiPBwgb5OSU1Teg';
+var serachFor = `the weekend`;
+var ResultNum = 1;
+
+// how you search for a video
+var search_http = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${ResultNum}&q=${serachFor}&key=${YOUTUBEAPIKEY}`;
+console.log('search_http: ', search_http);
+
+// how you get a video details
+
+var videoId = 'XXYlFuWEuKI';
+
+var video_http = `https://www.googleapis.com/youtube/v3/videos?part=snippet&maxResults=${ResultNum}&id=${videoId}&key=${YOUTUBEAPIKEY}`;
+console.log('video_http: ', video_http);
+
+// how you wathc the video
+
+var watch_http = `https://www.youtube.com/watch?v=${videoId}&part=snippet&maxResults=${ResultNum}key=${YOUTUBEAPIKEY}`;
+console.log('watch_http: ', watch_http);
