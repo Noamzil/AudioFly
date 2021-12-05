0b1c8288c9f29538e869c98f7426e036326dcbe9
Noam Zilberman <noam@zilberman.org>
2021-12-05 12:18:59 +0200

    startes liked songs page

---
+++ src/pages/liked-songs-page.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="liked-song-page">
+    <div class="liked-songs-description">
+      <img
+        src="https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"
+      />
+      <div class="description-txt">
+        <h2>PLAYLIST</h2>
+        <h1>Liked Songs</h1>
+        <div class="flex">
+          <p>user info </p>
+          <span> • </span>
+          <p class="songs-length">list length</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
