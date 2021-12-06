1c4a4352f4f70c317b479091d62c8a8a1690a210
Noam Zilberman <noam@zilberman.org>
2021-12-06 23:51:29 +0200

    fixed git problems, added queue page

--- src/components/app-header.cmp.vue
+++ src/components/app-header.cmp.vue
@@ -13,7 +13,7 @@
           </svg>
         </button>
       </div>
-      <div class="input-container">
+      <div class="input-container" v-if="route==='/search'">
         <svg role="img" class="search-icon-input" viewBox="0 0 24 24">
           <path
             d="M16.736 16.262A8.457 8.457 0 0019 10.5a8.5 8.5 0 10-3.779 7.067l4.424 5.18 1.521-1.299-4.43-5.186zM10.5 17C6.916 17 4 14.084 4 10.5S6.916 4 10.5 4 17 6.916 17 10.5 14.084 17 10.5 17z"
@@ -60,6 +60,11 @@ export default {
     },
     openModal(type) {
       eventBus.$emit('openModal', type)
+    }
+  },
+  computed:{
+    route() {
+      return this.$route.path
     }
   },
   components: {
---
+++ src/components/queue-cmps/song-queue-preview.cmp.vue
@@ -0,0 +1,71 @@
+<template>
+  <article
+    class="song-container"
+    @mouseover="isHover = true"
+    @mouseleave="isHover = false"
+  >
+    <button v-if="isHover" @click="$emit('playSong', song)" class="play-btn">
+      <svg role="img" viewBox="0 0 24 24">
+        <polygon
+          points="21.57 12 5.98 3 5.98 21 21.57 12"
+          fill="currentColor"
+        ></polygon>
+      </svg>
+    </button>
+    <p class="index" v-else>{{ songNum }}</p>
+
+    <div class="song-details">
+      <img class="song-img" :src="song.img" />
+      <p>{{ song.title }}</p>
+    </div>
+
+    <div class="song-options">
+      <div class="heart-container">
+        <section v-if="isHover || isSongLiked">
+          <button
+            @click="$emit('toggleLikeSong', song)"
+            class="like-btn fa-heart"
+            :class="isSongLiked ? 'fas btn-liked' : 'far'"
+          ></button>
+        </section>
+      </div>
+      <p>{{ songTime }}</p>
+      <div class="dots-container">
+        <div v-if="isHover">• • •</div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+import { utilService } from "../../services/util.service.js";
+
+export default {
+  props: ["song", "songNum"],
+  data() {
+    return {
+      showOps: false,
+      isHover: false,
+    };
+  },
+  computed: {
+    songTime() {
+      return utilService.writeTime(this.song.time);
+    },
+    isSongLiked() {
+      const userLiked = this.$store.getters.user.liked.song;
+      const isLiked = userLiked.find(
+        (song) => song.youtubeId === this.song.youtubeId
+      );
+      return isLiked ? true : false;
+    },
+    dateAdded() {
+      var addedAt = this.song.addedAt;
+      return addedAt.slice(0, 10);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
