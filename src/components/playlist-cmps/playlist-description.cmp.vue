f56393f072b46263c4dd3b902f276f3c34f4b8df
Or Ziskovich <zisko1992@gmail.com>
2021-12-06 14:37:32 +0200

    a

--- src/components/playlist-cmps/playlist-description.cmp.vue
+++ src/components/playlist-cmps/playlist-description.cmp.vue
@@ -16,10 +16,8 @@
       <div class="playlist-songs-details">
         <a>{{ currPlaylist.createdBy }}</a>
         <span class="dot"> • </span>
-        <span
-          >{{ currPlaylist.songs.length }} songs <span class="dot"> • </span> 00:00
-          (cpmputed album time)</span
-        >
+        <span>{{ currPlaylist.songs.length }} songs <span class="dot"> • </span> 00:00
+          (cpmputed album time)</span>
       </div>
     </div>
   </div>
