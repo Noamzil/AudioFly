9482d0a1a1ddb21d5fa8c314ce208969077b5628
Elior Papo <eliorpapo3@gmail.com>
2021-12-06 13:31:37 +0200

    new youtube

--- src/components/footer-cmps/play-song.cmp.vue
+++ src/components/footer-cmps/play-song.cmp.vue
@@ -8,8 +8,8 @@
 
 <script>
 export default {
-  name: "song-player",
-  props:['isPlaying'],
+  name: 'song-player',
+  props: ['isPlaying'],
   data() {
     return {
       isPlay: this.isAtPlay,
@@ -18,34 +18,37 @@ export default {
   },
   created() {
     this.openYtPlayer();
+    console.log(`1`);
+    var video = document.getElementById('player');
+    console.log(video);
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     pasueSong() {
       player.contentWindow.postMessage(
-        JSON.stringify({ event: "command", func: "pauseVideo" }),
-        "*"
+        JSON.stringify({ event: 'command', func: 'pauseVideo' }),
+        '*'
       );
     },
     playSong() {
       player.contentWindow.postMessage(
-        JSON.stringify({ event: "command", func: "playVideo" }),
-        "*"
+        JSON.stringify({ event: 'command', func: 'playVideo' }),
+        '*'
       );
     },
     openYtPlayer() {
+      console.log(`in`);
       var songId = this.$store.getters.currSong.youtubeId;
-      var tag = document.createElement("script");
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName("script")[0];
+      var tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
       var player;
       window.onYouTubePlayerAPIReady = function () {
-        player = new YT.Player("player", {
-          height: "390",
-          width: "640",
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
           videoId: songId,
           playerVars: {
             playsinline: 1,
