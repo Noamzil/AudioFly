e5fdc10163585f565d4c0626d4522af6296b5971
Noam Zilberman <noam@zilberman.org>
2021-12-07 17:06:29 +0200

    genre page css

--- src/pages/genre-page.vue
+++ src/pages/genre-page.vue
@@ -2,14 +2,14 @@
   <section class="gerne-page">
     <h1 class="genre-name">{{genre}}</h1>
     <div class="popular-playlists">
-      <div>
+      <div class="playlists-info">
         <h2>Popular playlists</h2>
         <h3>SEE ALL</h3>
         <!-- <genre-songs-list /> -->
       </div>
     </div>
     <div class="new-releases">
-      <div>
+      <div class="playlists-info">
         <h2>New releases</h2>
         <h3>SEE ALL</h3>
       </div>
