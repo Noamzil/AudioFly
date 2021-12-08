ee6d2d582b403da475b7a6392a163000bab590c3
Noam Zilberman <noam@zilberman.org>
2021-12-08 18:13:57 +0200

    added next/prev page, queue, fixed edit playlist modal

---
+++ src/components/playlist-cmps/playlist-options.cmp.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="playlist-linear-ops">
+    <ul>
+      <li @click="share">Share</li>
+      <li @click="addToQueue">Add to queue</li>
+      <li @click="editPlaylist">Edit Playlist</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "playlist-options",
+  //   props: ['song'],
+  methods: {
+    // removeSong() {
+    //   this.$emit('removeSong', this.song);
+    // },
+    addToQueue() {
+      console.log(`addToQueue playlist`);
+      this.currPlayList.songs.forEach((song) => {
+        var entity = song;
+        this.$store.commit({ type: "addToQueue", entity });
+      })
+    },
+    share() {
+      console.log(`share`);
+    },
+    editPlaylist() {
+      this.$emit('editPlaylist')
+    }
+  },
+  computed: {
+    currPlayList() {
+      const playlistId = this.$route.params;
+      const playlists = this.$store.getters.playlists;
+      var currPlayList = playlists.find(
+        (playlist) => (playlist._id = playlistId)
+      );
+      return currPlayList;
+    },
+  },
+};
+</script>
