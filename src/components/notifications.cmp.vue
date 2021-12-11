5d133430d778f27cbc75210b8e48ff26e69b623e
Noam Zilberman <noam@zilberman.org>
2021-12-11 16:47:53 +0200

    added notifications

---
+++ src/components/notifications.cmp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="notifications">
+    <ul>
+      <li
+        v-for="invitation in invitations"
+        :key="invitation.id"
+        @click="goToStation(invitation.playlist)"
+      >
+        <notification-prev :invitation="invitation" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import NotificationPrevCmp from "./notification-prev.cmp.vue";
+import notificationPrev from "./notification-prev.cmp.vue";
+export default {
+  name: "notifications",
+  props: ["invitations"],
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {
+      goToStation(stationtId) {
+                this.$router.push("/playlist/" + stationtId);
+                this.$emit('toggleNotification')
+      }
+  },
+  components: {
+    notificationPrev,
+  },
+};
+</script>
