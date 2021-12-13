77fc012d270e010435f2d5e918e7c33087172a18
Or Ziskovich <zisko1992@gmail.com>
2021-12-13 21:19:36 +0200

    m

--- src/components/side-bar.cmp.vue
+++ src/components/side-bar.cmp.vue
@@ -31,7 +31,7 @@
             d="M13.66 4.097l-.913.406 7.797 17.513.914-.406L13.66 4.097zM3 22h1V4H3v18zm6 0h1V4H9v18z"
           ></path>
         </svg>
-        <h3>Your Library</h3>
+        <h3>Library</h3>
       </router-link>
     </div>
 
